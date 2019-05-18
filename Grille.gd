d26904c147849cbe6f21e4a654d1025d39cd1429
Hugo <hugopeyre66@gmail.com>
2019-05-18 02:25:37 +0200

    HUD

--- Grille.gd
+++ Grille.gd
@@ -3,14 +3,12 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var i = 0
-var j = 0
-var a = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in range(4):
 		for j in range(9):
-			var carreau_resource = preload("carreau.tscn")
+			var carreau_resource = preload("Carreau.tscn")
 			var carreau = carreau_resource.instance()
 			carreau.position = Vector2(j*100+100, i*100+100) # use set_translation() if you are in 3D
 			if j<5: 
