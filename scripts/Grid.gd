0b7de69a5c7071d3bab81476ce1d2a21842687be
_Map_ <blendfan@gmail.com>
2019-05-19 11:48:48 +0200

    Little update

---
+++ scripts/Grid.gd
@@ -0,0 +1,27 @@
+extends Spatial
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var cells_grid = null
+var light_grid = null
+
+func init_grids(lg_size = 9, cg_size = 3):
+	light_grid = []
+	for i in range(lg_size):
+		light_grid.append([])
+		for j in range(lg_size):
+			light_grid.append(null)
+	cells_grid = []
+	for i in range(lg_size * cg_size):
+		cells_grid.append([])
+		for j in range(lg_size * cg_size):
+			cells_grid.append(null)
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
