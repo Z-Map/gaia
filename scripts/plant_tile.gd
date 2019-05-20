cab94545ad4bb726b526b5e02b237f02a37edc5c
_Map_ <blendfan@gmail.com>
2019-05-20 04:39:57 +0200

    Coucou forêt

--- scripts/plant_tile.gd
+++ scripts/plant_tile.gd
@@ -1,7 +1,13 @@
 extends Spatial
 
-export(int) var growth_lvl1:int = 60
-export(int) var death_lvl1:int = 24
+export(int) var growth_lvl1:int = 30
+export(int) var death_lvl1:int = 15
+
+export(int) var growth_lvl2:int = 60
+export(int) var death_lvl2:int = 12
+
+export(int) var growth_lvl3:int = 20
+export(int) var death_lvl3:int = 15
 
 var grid = null
 
@@ -17,6 +23,7 @@ var current_level
 func set_light(v = false):
 	$lvl1.change_layer(v)
 	$lvl2.change_layer(v)
+	$lvl3.change_layer(v)
 	if current_level and v != light:
 		var adv = current_level.growth
 		var time
@@ -25,35 +32,58 @@ func set_light(v = false):
 		if v:
 			if lvl == 0:
 				time = growth_lvl1
+			elif lvl == 1:
+				time = growth_lvl2
 			else:
-				time = 0
+				time = growth_lvl3
 			offset = adv * time
 			current_time = time
 			$growth.start(time - offset)
 		else:
 			if lvl == 0:
 				time = death_lvl1
+			elif lvl == 1:
+				time = death_lvl2
 			else:
-				time = 0
+				time = death_lvl3
 			offset = 0
 			current_time = time
 			$death.start(adv * time)
 	light = v
 
 func next_level():
+	$death.stop()
+	$growth.stop()
 	if lvl == 0:
-		current_level = $lvl1
+		current_level = $lvl2
+		current_time = growth_lvl2
+	elif lvl == 1:
+		current_level = $lvl3
+		current_time = growth_lvl3
 	else:
-		if lvl == 1:
-			current_level = $lvl2
+		return
+	offset = 0
+	$growth.start(current_time)
+	lvl += 1
 
 func prev_level():
+	$death.stop()
+	$growth.stop()
+	if lvl >= 3:
+		current_level = $lvl3
+		current_time = death_lvl3
 	if lvl == 2:
+		current_level = $lvl2
+		current_time = death_lvl2
+	elif lvl == 1:
 		current_level = $lvl1
+		current_time = death_lvl1
 	else:
-		if lvl == 1:
-			current_level = $lvl0
-
+		return
+	offset = 0
+	$death.start(current_time)
+	lvl -= 1
+	
 func refresh_shape():
 	pass
 
@@ -65,6 +95,7 @@ func set_pos(x, y):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	rotate_y(PI * rand_range(0.0, 2.0))
 	$lvl1.growth = 0
 	$lvl2.growth = 0
 	current_level = $lvl1
@@ -82,7 +113,6 @@ func _process(delta):
 func _on_growth_timeout():
 	grid.plant_ready(pos_x, pos_y)
 	next_level()
-	
 
 
 func _on_death_timeout():
